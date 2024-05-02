152911d5eaa4b6c00bcbb4fa9944ca767a7da854
m.zeylan <m.zeylan@webvork.com>
2024-05-02 15:30:06 +0300

    change api

--- app/src/main/java/com/test/searchfollowerstest/FollowersApi.kt
+++ app/src/main/java/com/test/searchfollowerstest/FollowersApi.kt
@@ -1,10 +1,21 @@
 package com.test.searchfollowerstest
 
+import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Part
+import retrofit2.http.Path
 import retrofit2.http.Query
+import retrofit2.http.Url
+
 
 interface FollowersApi {
 
     @GET("/search/users")
-    suspend fun getFollowers(@Query("q") query: String)
+    fun getUsers(@Query("q") query: String): Call<ItemList>
+
+    @GET()
+    fun loadFollowers(@Url url: String): Call<List<ItemFollowers>>
+
+    @GET
+    fun loadRepos(@Url url : String) : Call<List<ItemRepo>>
 }
